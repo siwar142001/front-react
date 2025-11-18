db515c4e352139ded8b09b83b705dca7e1b439c4
Ludwig <115760654+Cralak@users.noreply.github.com>
2025-11-18 16:26:03 +0100

    initial commit v2

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
