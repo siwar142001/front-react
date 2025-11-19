8f95e5d42b0895b04d6ff6f28b4174623fa13d24
Ludwig <115760654+Cralak@users.noreply.github.com>
2025-11-19 15:42:11 +0100

    tailwind fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
+import DeleteButton from "../components/utils/DeleteButton"
+
 export default function Dashboard(){
     return(
-        <div>
-            test
-        </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     )
 }
