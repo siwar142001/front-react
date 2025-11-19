d6c5066eebb592f8480f00a53aeeea32638c20c5
Ludwig <115760654+Cralak@users.noreply.github.com>
2025-11-19 15:11:41 +0100

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
